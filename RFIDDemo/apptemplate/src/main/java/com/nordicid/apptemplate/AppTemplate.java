425166bb5111182a5a18e305bf4e90060832eddb
Yassine Mounir <tychus.mounir@gmail.com>
2017-05-17 15:41:43 +0300

    Permission fix

--- RFIDDemo/apptemplate/src/main/java/com/nordicid/apptemplate/AppTemplate.java
+++ RFIDDemo/apptemplate/src/main/java/com/nordicid/apptemplate/AppTemplate.java
@@ -1,11 +1,11 @@
 package com.nordicid.apptemplate;
 
+import java.io.File;
 import java.util.ArrayList;
-import java.util.concurrent.ExecutionException;
+import java.util.List;
 
 import com.nordicid.nuraccessory.NurAccessoryExtension;
 import com.nordicid.nurapi.NurApi;
-import com.nordicid.nurapi.NurApiException;
 import com.nordicid.nurapi.NurApiListener;
 import com.nordicid.nurapi.NurApiUiThreadRunner;
 import com.nordicid.nurapi.NurEventAutotune;
@@ -23,6 +23,8 @@ import com.nordicid.nurapi.NurEventTraceTag;
 import com.nordicid.nurapi.NurEventTriggeredRead;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -63,10 +65,7 @@ public class AppTemplate extends FragmentActivity {
 
 	private SubAppList mSubAppList;
 
-    private final int FINE_LOCATION_REQ_CODE = 40;
-	private final int COARSE_LOCATION_REQ_CODE = 41;
-    private final int READ_EXTERNAL_STORAGE_REQ_CODE = 44;
-    private final int WRITE_EXTERNAL_STORAGE_REQ_CODE = 45;
+	private final int APP_PERMISSION_REQ_CODE = 41;
 
 	private DrawerLayout mDrawerLayout;
 	private FrameLayout mMenuContainer;
@@ -240,40 +239,26 @@ public class AppTemplate extends FragmentActivity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		/** Bluetooth Permission checks **/
-		if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-			if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
-				/** ? ? ? **/
-			} else {
-				ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},COARSE_LOCATION_REQ_CODE);
-			}
-		}
-
-		if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-			if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-				/** ? ? ? **/
-			} else {
-				ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},FINE_LOCATION_REQ_CODE);
-			}
-		}
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                /** ? ? ? **/
-            } else {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},READ_EXTERNAL_STORAGE_REQ_CODE);
-            }
-        }
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                /** ? ? ? **/
-            } else {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},WRITE_EXTERNAL_STORAGE_REQ_CODE);
-            }
-        }
-
 		gInstance = this;
+		/** Bluetooth Permission checks **/
+		if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+				ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+			 	ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+				ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+
+			if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION) ||
+					ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION) ||
+					ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE) ||
+					ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+					) {
+				/** ? ? ? **/
+			} else {
+				ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+																		Manifest.permission.ACCESS_FINE_LOCATION,
+																		Manifest.permission.READ_EXTERNAL_STORAGE,
+																		Manifest.permission.WRITE_EXTERNAL_STORAGE}, APP_PERMISSION_REQ_CODE);
+			}
+		}
 
 		super.onCreate(savedInstanceState);
 		//int screenLayout = 0;
@@ -335,26 +320,30 @@ public class AppTemplate extends FragmentActivity {
 	}
 
 	@Override
-	public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+	public void onRequestPermissionsResult(int requestCode, final String permissions[], final int[] grantResults) {
+		final List<String> missingPermissions = new ArrayList<>();
 		switch (requestCode) {
-			case FINE_LOCATION_REQ_CODE:
-			case COARSE_LOCATION_REQ_CODE: {
-				// If request is cancelled, the result arrays are empty.
-				if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-				} else {
-					Toast.makeText(this,"Applioation will not connect to bluetooth devices without these permissions",Toast.LENGTH_SHORT).show();
+			case APP_PERMISSION_REQ_CODE: {
+				for (int i = 0; i < grantResults.length; i++) {
+					if(grantResults[i] == PackageManager.PERMISSION_DENIED)
+						missingPermissions.add(permissions[i]);
 				}
-				return;
+				if (missingPermissions.size() > 0) {
+					AlertDialog.Builder builder = new AlertDialog.Builder(this);
+					builder.setMessage("Application will not work properly without all requested permissions. Do you want to grant them now ?")
+							.setPositiveButton("Grant Permissions", new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog, int id) {
+									ActivityCompat.requestPermissions(gInstance, missingPermissions.toArray(new String[0]), APP_PERMISSION_REQ_CODE);
+								}
+							})
+							.setNegativeButton("No thanks", new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog, int id) {
+									Toast.makeText(gInstance,"Missing permissions",Toast.LENGTH_SHORT).show();
+								}
+							});
+					builder.create().show();
+				}
 			}
-            case READ_EXTERNAL_STORAGE_REQ_CODE:
-            case WRITE_EXTERNAL_STORAGE_REQ_CODE: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                } else {
-                    Toast.makeText(this,"Application will not be able to execute some features",Toast.LENGTH_SHORT).show();
-                }
-                return;
-            }
 		}
 	}
 
